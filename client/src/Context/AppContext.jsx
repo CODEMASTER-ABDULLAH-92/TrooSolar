7b6c7c02e24a0c873e23091c4a8fa95db9a4a6e0
CODEMASTER-ABDULLAH-92 <abdullahpeerzada92@gmail.com>
2025-07-13 21:49:36 +0500

    Update AppContext.jsx

--- client/src/Context/AppContext.jsx
+++ client/src/Context/AppContext.jsx
@@ -3,22 +3,24 @@ import { createContext, useState } from "react";
 export const ContextApi = createContext();
 
 export const ContextProvider = (props) => {
+    
     let [quantity,setQuantity] = useState({});
-    const value = {
-        quantity,setQuantity,
-    }
-
+    
     const addToCart = (itemId) => {
         let cartData = structuredClone(quantity);
-    
+        
         if (!cartData[itemId]) {
             cartData[itemId] = 1;
         } else {
             cartData[itemId] = cartData[itemId] + 1;
         }
-    
+        
         setQuantity(cartData);
     };
+    
+    const value = {
+        quantity,setQuantity,addToCart
+    }
     return (
         <ContextApi.Provider value={value}>
             {props.children}
